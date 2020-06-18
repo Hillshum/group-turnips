da47e4b137598fd5c0ed3bca4c27076ea314b95a
Hilton Shumway <hillshum@gmail.com>
2020-06-18 17:53:09 -0500

    Add basic collapsing on inputs

--- src/components/price-inputs/price-inputs.tsx
+++ src/components/price-inputs/price-inputs.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Inputs } from '../../models';
 
+import './price-inputs.scss';
+
 const PRICES = [
   'Sunday',
   'Monday AM',
@@ -25,27 +27,34 @@ export interface InputProps {
 }
 
 const PriceInputs = ({ name, inputs, onChange }: InputProps) => {
+  const [isCollapsed, setCollapsed] = React.useState(true);
   return (
     <div className="price-inputs">
-      <div>{name}</div>
-      {PRICES.map((price, index) => (
-        <div className="price-input" key={price}>
-          <label>
-            {price}
-            <input
-              type="number"
-              value={inputs.prices[index]}
-              onChange={(e) => {
-                const newinputs = { ...inputs, prices: [...inputs.prices] };
-                newinputs.prices[index] = e.target.value
-                  ? Number(e.target.value)
-                  : undefined;
-                onChange(newinputs);
-              }}
-            />
-          </label>
-        </div>
-      ))}
+      <div className="inputs-header">
+        <span>{name}</span>
+        <button onClick={() => setCollapsed(!isCollapsed)}>
+          {isCollapsed ? 'Show Prices' : 'Hide Prices'}
+        </button>
+      </div>
+      {!isCollapsed &&
+        PRICES.map((price, index) => (
+          <div className="price-input" key={price}>
+            <label>
+              {price}
+              <input
+                type="number"
+                value={inputs.prices[index]}
+                onChange={(e) => {
+                  const newinputs = { ...inputs, prices: [...inputs.prices] };
+                  newinputs.prices[index] = e.target.value
+                    ? Number(e.target.value)
+                    : undefined;
+                  onChange(newinputs);
+                }}
+              />
+            </label>
+          </div>
+        ))}
     </div>
   );
 };
