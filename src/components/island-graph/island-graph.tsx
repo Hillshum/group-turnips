ecc2fcebdfcf1c298bc56637e886179823b4e949
Hilton Shumway <hillshum@gmail.com>
2020-07-22 13:39:17 -0500

    Fix graph a bit
    Accidently commited some of this earlier

--- src/components/island-graph/island-graph.tsx
+++ src/components/island-graph/island-graph.tsx
@@ -12,7 +12,7 @@ const IslandGraph = ({ predictions }: Props) => {
   return (
     <LineChart data={data} width={300} height={200}>
       <CartesianGrid />
-      <XAxis dataKey="name" interval={1} />
+      <XAxis dataKey="name" />
       <YAxis width={30} domain={[0, 700]} />
       <Line type="monotone" dataKey="min" />
       <Line type="monotone" dataKey="max" />
