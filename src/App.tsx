3d538a75c2b256f1fa4337d78caa843985f55ae0
Hilton Shumway <hillshum@gmail.com>
2024-11-16 12:12:18 -0600

    WIP on adding auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.scss';
 
 import { useCollection } from 'react-firebase-hooks/firestore';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { collection, query, updateDoc } from "firebase/firestore";
 
 import IslandOverview from './components/island-overview/island-overview';
 import { Inputs, Prediction } from './models';
 import PriceInputs from './components/price-inputs/price-inputs';
-import { firestore } from './api/firebase';
+import { firestore, auth } from './api/firebase';
 import { PredictionStore } from './api/predictionContext';
 import ProbabilitySummary from './components/probability-summary/probability-summary';
 
@@ -29,9 +30,17 @@ function App() {
     }));
   };
 
+  const [login, user, loginLoading, loginError ] = useSignInWithGoogle(auth)
+
+
   return (
     <div className="App">
       <ProbabilitySummary predictions={predictions} />
+      <div className="login-status">
+        { !user && <button onClick={()=>login()}>Sign in</button> }
+        { loginLoading && <div>Logging in</div>}
+        { user && <div>{user.user.email}</div>}
+      </div>
       {islands?.docs.map((island) => {
         const { name, ...inputs } = island.data() as Island;
         return (
