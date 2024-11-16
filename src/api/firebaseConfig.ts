96734a81c0602c0a8927158f9d7034e1a813c22e
Hilton Shumway <hillshum@gmail.com>
2024-11-16 12:42:13 -0600

    Add a README and other polishing steps

--- src/api/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: 'AIzaSyD_Ihbizw-IFGrIYNvG5eETj48z8JH3qHg',
-  authDomain: 'group-turnips.firebaseapp.com',
-  databaseURL: 'https://group-turnips.firebaseio.com',
-  projectId: 'group-turnips',
-  storageBucket: 'group-turnips.appspot.com',
-  messagingSenderId: '56755990376',
-  appId: '1:56755990376:web:9b1638609212f4a3cbb0dd',
-};
